3ef3137a3c48c7ef0e0b4391de31691cde840c18
arandombit <a.random.luis@proton.me>
2026-01-08 22:43:12 -0500

    update button color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import confetti from 'canvas-confetti'
 export default props => (
   <button
     onClick={() => confetti()}
-    className='appearance-none py-2 px-4 bg-purple-500 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-75'
+    className='appearance-none py-2 px-4 bg-blue-400 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:ring-opacity-75'
   >
     { props.children }
   </button>
