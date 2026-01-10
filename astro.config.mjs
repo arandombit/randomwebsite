b39a4546991aaaf81a5811f60c185a1db18c80f4
arandombit <a.random.luis@proton.me>
2026-01-09 19:02:36 -0500

    modify astro config spacing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [react()],
+  vite: { plugins: [tailwindcss()] },
+  integrations: [react()]
 });
