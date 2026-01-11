da4775c0be3dbc22fbb909dba446408c422a3022
arandombit <a.random.luis@proton.me>
2026-01-10 23:15:47 -0500

    remove call to confetti function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,5 @@
 export default props => (
-  <button
-    onClick={() => confetti()}
+  <button {...props}
     className='appearance-none py-2 px-4 bg-blue-400 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:ring-opacity-75'
   >
     { props.children }
